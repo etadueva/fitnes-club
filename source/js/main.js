de374bf878c6a3cd1c74cc854f4b07868bcbf0ce
EkaterinaTadueva <etadueva@bk.ru>
2022-12-20 20:08:24 +0300

    Добавила шрифты

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 
 // ---------------------------------
 
@@ -16,7 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
   });
 });
 
